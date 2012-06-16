b23677f6b6ad4b5d2f866eb0aadd689bdf0ee9fd
Thomas De Waelheyns <thomas.dewaelheyns@gmail.com>
2012-06-16 12:57:04 +0200

    Started working on the file loader. Also added a thanks.txt. In it we can thank people for their contribution or code we reused under license.

---
+++ dataHandling/FileHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hostWin.dataHandling
+{
+    interface FileHandler
+    {
+        void loadFile(string path, string fileName);
+        string[] getFile();
+        void backupFile();
+        void writeFile(string[] content);
+    }
+}
---
+++ dataHandling/WindowsFileHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hostWin.dataHandling
+{
+    class WindowsFileHandler: FileHandler 
+    {
+
+        public void loadFile(string path, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] getFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void backupFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void writeFile(string[] content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
